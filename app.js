32c95486c00b088732017899e09bb9c779f444c7
alimonse <alimonse29@gmail.com>
2017-11-17 17:02:20 -0500

    conectadon

--- app.js
+++ app.js
@@ -376,9 +376,24 @@ request.on('response', function(response) {
 		const result = response.result;
 		if (result){
 			const fulfillment = result.fulfillment;
-			if(fulfillment && fulfillment.speech && fulfillment.speech.length>0){
+			if(fulfillment&&fulfillment.speech&&fulfillment.speech.length>0){
 				sendTextMessage(senderID,fulfillment.speech);
 			}
+			else {
+				const action = result.action;;
+				const parameters = result.parameters;
+				console.log('action: ', action);
+				console.log('parameters: ',parameters);
+				switch(action){
+					case 'account.balance':
+					sendTextMessage(senderID,'get account balance');
+					break;
+					case 'account.movement':
+					sendTextMessage(senderID,'get account movement');
+					break;
+					default:
+					console.log('unknown action...');
+					break;
 		}
 	}
 });
