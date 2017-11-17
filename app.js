48e377fc42925e3da9024b598deb9cf9718f4c69
alimonse <alimonse29@gmail.com>
2017-11-17 17:24:27 -0500

    ori

--- app.js
+++ app.js
@@ -386,7 +386,6 @@ request.on('response', function(response) {
 				console.log('parameters: ',parameters);
 				switch(action){
 					case 'account.balance':
-					console.log('Balance');
 					sendTextMessage(senderID,'get account balance');
 					break;
 					case 'account.movement':
